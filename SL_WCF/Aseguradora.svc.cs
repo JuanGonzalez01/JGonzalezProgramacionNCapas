68d0be0ef96ed0ccd11a55933120b158eed07fae
JuanGonzalez01 <jalbertogonzg@gmail.com>
2022-12-07 11:01:48 -0600

    Operaciones y CRUD WCF

---
+++ SL_WCF/Aseguradora.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SL_WCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Aseguradora" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Aseguradora.svc o Aseguradora.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Aseguradora : IAseguradora
+    {
+        public SL_WCF.Result GetAll()
+        {
+            ML.Result result = BL.Aseguradora.GetAllEF();
+            return new SL_WCF.Result { Correct=result.Correct, ErrorMessage=result.ErrorMessage, Ex=result.Ex, Object=result.Object, Objects=result.Objects};
+        }
+
+        public SL_WCF.Result GetById(int idAseguradora)
+        {
+            ML.Result result = BL.Aseguradora.GetByIdEF(idAseguradora);
+            return new SL_WCF.Result { Correct = result.Correct, ErrorMessage = result.ErrorMessage, Ex = result.Ex, Object = result.Object, Objects = result.Objects };
+        }
+
+        public SL_WCF.Result Update(ML.Aseguradora aseguradora)
+        {
+            ML.Result result = BL.Aseguradora.UpdateEF(aseguradora);
+            return new SL_WCF.Result { Correct = result.Correct, ErrorMessage = result.ErrorMessage, Ex = result.Ex, Object = result.Object, Objects = result.Objects };
+        }
+
+        public SL_WCF.Result Add(ML.Aseguradora aseguradora)
+        {
+            ML.Result result = BL.Aseguradora.AddEF(aseguradora);
+            return new SL_WCF.Result { Correct = result.Correct, ErrorMessage = result.ErrorMessage, Ex = result.Ex, Object = result.Object, Objects = result.Objects };
+        }
+
+        public SL_WCF.Result Delete(int idAseguradora)
+        {
+            ML.Result result = BL.Aseguradora.DeleteEF(idAseguradora);
+            return new SL_WCF.Result { Correct = result.Correct, ErrorMessage = result.ErrorMessage, Ex = result.Ex, Object = result.Object, Objects = result.Objects };
+        }
+    }
+}
---
+++ SL_WCF/IAseguradora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SL_WCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IAseguradora" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IAseguradora
+    {
+        [OperationContract]
+        [ServiceKnownType(typeof(ML.Aseguradora))]
+        SL_WCF.Result GetAll();
+
+        [OperationContract]
+        [ServiceKnownType(typeof(ML.Aseguradora))]
+        SL_WCF.Result GetById(int idAseguradora);
+
+        [OperationContract]
+        SL_WCF.Result Add(ML.Aseguradora aseguradora);
+
+        [OperationContract]
+        SL_WCF.Result Update(ML.Aseguradora aseguradora);
+
+        [OperationContract]
+        SL_WCF.Result Delete(int idAseguradora);
+
+    }
+}
